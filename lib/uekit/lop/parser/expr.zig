927a770d2f29d63f83af90173ac49c69a2f214a4
Tristan Ross <tristan.ross@midstall.com>
2023-12-04 18:11:41 -0800

    feat(lop): better expression formatting

--- lib/uekit/lop/parser/expr.zig
+++ lib/uekit/lop/parser/expr.zig
@@ -30,19 +30,19 @@ pub const Expression = union(enum) {
         _ = options;
 
         try writer.writeAll(@typeName(Expression));
-        try writer.print("{{ .{s} = ", .{ @tagName(std.meta.activeTag(self)) });
+        try writer.print("{{ .{s} = ", .{@tagName(std.meta.activeTag(self))});
 
         switch (self) {
-            .unsignedNumber => |un| try writer.print("{} ", .{ un }),
-            .signedNumber => |sn| try writer.print("{} ", .{ sn }),
-            .builtin => |bt| try writer.print("{} ", .{ bt }),
-            .string, .literal => |sl| try writer.print("{s} ", .{ sl }),
+            .unsignedNumber => |un| try writer.print("{} ", .{un}),
+            .signedNumber => |sn| try writer.print("{} ", .{sn}),
+            .builtin => |bt| try writer.print("{} ", .{bt}),
+            .string, .literal => |sl| try writer.print("{s} ", .{sl}),
             .instruction => |inst| {
-                try writer.print("{{ .opcode = {} ", .{ inst.opcode });
+                try writer.print("{{ .opcode = {}", .{inst.opcode});
 
                 if (inst.operands) |operands| {
-                    try writer.print(", .operands = {any} ", .{ operands.items });
-                }
+                    try writer.print(", .operands = {any} ", .{operands.items});
+                } else try writer.writeByte(' ');
 
                 try writer.writeAll("}");
             },
